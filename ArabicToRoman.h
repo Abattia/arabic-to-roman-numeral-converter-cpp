9d96684d29322e57488ab8d8efa0d8a646f3222e
Ben Van Every <ben.vanevery@gmail.com>
2019-04-01 10:22:56 +0200

    After 30 mins

--- ArabicToRoman.h
+++ ArabicToRoman.h
@@ -4,8 +4,21 @@
 using std::string;
 using std::map;
 
-map <int, string> symbol = {{10,"X"},{1,"I"}};
-int levels[] = {10, 1};
+map <int, string> symbol = {{1000,"M"}, 
+				{900, "CM"}, 
+				{500, "D"}, 
+				{400, "CD"},
+				{100, "C"}, 
+				{90, "XC"},
+				{50, "L"}, 
+				{40, "XL"}, 
+				{10, "X"}, 
+				{9, "IX"}, 
+				{5, "V"}, 
+				{4, "IV"}, 
+				{1, "I"}};
+
+int levels[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9,5, 4, 1};
 
 string ToRoman(int anArabic) {
 	string result = string();
